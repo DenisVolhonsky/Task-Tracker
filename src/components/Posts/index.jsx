4d8fc2c350cd7416283f60a8d229efd9dda488ec
Anastasiia <nastyailina2112@gmail.com>
2018-04-01 13:23:09 +0300

    UI for main taskmanager page

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './style.css'
+import garbage from './garbage.svg';
+import star from './star.svg';
+import done from './done.svg';
 
 const Posts = ({id, title, onTodoClick}) => ( // сразу деструктуризируем props
     <div className="posts">
-        <h3><a className="posts__link" href="">{title}</a></h3>
-        <span className="delete-btn" onClick={()=>onTodoClick(id)}></span>
+
+        <p>
+        <a className="posts__link" href=""><img className = "done" src = {done} alt="" />{title}</a>
+        <img className = "str" src = {star} alt="" />
+        <img className = "garb" src = {garbage} alt="" onClick={()=>onTodoClick(id)}/>
+        </p>
+
     </div>
 );
 
