22b09941e9063c584617f78da70c45bb2971dfed
Anastasiia <nastyailina2112@gmail.com>
2018-04-01 17:58:42 +0300

    change css

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -8,7 +8,7 @@ import done from './done.svg';
 const PostItem = ({id, text, onDelete }) => (
   <div className="post-item">
     <a className="posts__link" href="">
-      <img className="done" src={done} alt=""/>
+       &#10004;
     </a>
     <div className="post-item__container">
       <p className="post-item__text">{ text }</p>
@@ -30,3 +30,30 @@ PostItem.propTypes = {
 
 export default PostItem;
 
+
+
+
+// const Posts = ({id, title, onTodoClick}) => (
+//   <div className="post-item">
+//     <a className="posts__link" href="">
+//       &#10004;
+//     </a>
+//     <div className="post-item__container">
+//       <p className="post-item__text">{title}</p>
+//       <div className="post-item__info">
+//         <p className="post-item__time">13:55</p>
+//         <img className="str" src={star} alt=""/>
+//         <img className="garb" src={garbage} alt=""
+//              onClick={() => onTodoClick(id)}
+//         />
+//       </div>
+//     </div>
+//   </div>
+// );
+
+// Posts.propTypes = {
+//   id: PropTypes.string.isRequired,
+//   title: PropTypes.string.isRequired,
+// };
+
+// export default Posts;
