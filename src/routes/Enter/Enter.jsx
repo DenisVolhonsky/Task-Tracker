c4f3f3d599e11ff2e5f0a8217c317ac9d15e02f2
ivanlapa <sstage44@gmail.com>
2018-04-01 13:33:26 +0300

    Change login btn name

--- src/routes/Enter/Enter.jsx
+++ src/routes/Enter/Enter.jsx
@@ -5,7 +5,7 @@ import './style.css';
 const Enter = () => {
 	return (
 		<div className = "center-align card-panel hoverable">
-			<Link to="/register" className="btn-large">Sing in</Link>
+			<Link to="/register" className="btn-large">Sing up</Link>
 			<Link to="/login" className="btn-large">Log in</Link>
 
 		</div>
